3d962f105aced3a5c43656f7b793e8ab402d7019
Arjun Satarkar <me@arjunsatarkar.net>
2025-03-03 15:56:39 -0500

    mpvclip: also use current video and audio track, fix bugs

--- mpvclip/main.lua
+++ mpvclip/main.lua
@@ -3,9 +3,9 @@ local input = require "mp.input"
 local function log_cmd(args)
     local cmd = ""
     for _, v in ipairs(args) do
-        cmd = cmd .. string.format("%q", v) .. ", "
+        cmd = cmd .. " " .. string.format("%s", v)
     end
-    print("Running command: " .. cmd)
+    print("Running command:" .. cmd)
 end
 
 local function extend(arr1, arr2)
@@ -22,14 +22,32 @@ local function copy_arr(arr)
     return result
 end
 
-local function do_clip(a, b, crf, two_pass_target, sub_track_id, path)
+local function do_clip(a, b, crf, two_pass_target, video_track_id, audio_track_id, sub_track_id, path)
     local AUDIO_CODEC = "libopus"
     local AUDIO_BITRATE_KIBIBITS = 128
-    local AUDIO_BITRATE_STR = tostring(AUDIO_BITRATE_KIBIBITS) .. "k"
+    local AUDIO_BITRATE = math.floor(AUDIO_BITRATE_KIBIBITS * 1024)
+    local AUDIO_BYTES_PER_SECOND = math.floor(AUDIO_BITRATE / 8)
+    local FILTERGRAPH_V_OUT = "v_out"
 
-    local filterchain = nil
+    local video_input_pad = "0:v:" .. tostring(video_track_id - 1)
+
+    local audio_input_pad = ""
+    if audio_track_id then
+        audio_input_pad = "0:a:" .. tostring(audio_track_id - 1)
+    end
+
+    local filtergraph = nil
     if sub_track_id then
-        filterchain = string.format("subtitles='%s':si=%d", path, sub_track_id - 1)
+        --[[
+        Not sure why this is the correct number of escapes, but it works.
+        Ref: https://stackoverflow.com/a/10729560
+        (slightly different context and approach, but helpful)
+        --]]
+        local squote_escaped_path = path:gsub("'", [['\\\'']])
+        filtergraph = string.format(
+            "[%s]subtitles='%s':si=%d[%s]",
+            video_input_pad, squote_escaped_path, sub_track_id - 1, FILTERGRAPH_V_OUT
+        )
     end
 
     local out_path = string.format("clip_%d.mp4", os.time())
@@ -46,18 +64,31 @@ local function do_clip(a, b, crf, two_pass_target, sub_track_id, path)
         "-ss", a,
         "-to", b,
     }
-    if filterchain then
-        extend(base_args, { "-filter_complex", filterchain })
+    if filtergraph then
+        extend(base_args, {
+            "-filter_complex", filtergraph,
+            "-map", ("[%s]"):format(FILTERGRAPH_V_OUT)
+        })
+    else
+        extend(base_args, { "-map", video_input_pad })
+    end
+
+    local audio_args = {}
+    if audio_track_id then
+        audio_args = {
+            "-c:a", AUDIO_CODEC,
+            "-b:a", tostring(AUDIO_BITRATE),
+            "-map", audio_input_pad
+        }
     end
 
     if crf then
         local args = copy_arr(base_args)
+        extend(args, audio_args)
         extend(args, {
             "-crf", crf,
             "-pix_fmt", "yuv420p",
             "-movflags", "+faststart",
-            "-c:a", AUDIO_CODEC,
-            "-b:a", AUDIO_BITRATE_STR,
             out_path
         })
 
@@ -66,7 +97,7 @@ local function do_clip(a, b, crf, two_pass_target, sub_track_id, path)
     elseif two_pass_target then
         local clip_secs = (tonumber(b) - tonumber(a))
         local total_bytes = two_pass_target * 1024 * 1024
-        local audio_bytes = AUDIO_BITRATE_KIBIBITS * (1024 / 8) * clip_secs
+        local audio_bytes = AUDIO_BYTES_PER_SECOND * clip_secs
         local video_bytes = total_bytes - audio_bytes
         local video_bitrate = video_bytes / clip_secs * 8
 
@@ -91,19 +122,19 @@ local function do_clip(a, b, crf, two_pass_target, sub_track_id, path)
         mp.command_native({ name = "subprocess", args = args })
 
         args = copy_arr(base_args)
+        extend(args, audio_args)
         extend(args, {
             "-b:v", tostring(video_bitrate),
             "-pass", "2",
-            "-c:a", AUDIO_CODEC,
-            "-b:a", AUDIO_BITRATE_STR,
             out_path
         })
 
         log_cmd(args)
         mp.command_native({ name = "subprocess", args = args })
 
-        os.remove("ffmpeg2pass-0.log")
-        os.remove("ffmpeg2pass-0.log.mbtree")
+        local log_file_name = string.format("ffmpeg2pass-%d.log", video_track_id - 1)
+        os.remove(log_file_name)
+        os.remove(log_file_name .. ".mbtree")
     end
 
     local message = "Wrote clip to " .. out_path
@@ -121,6 +152,8 @@ local function get_params()
     end
 
     local path = mp.get_property("path")
+    local video_track_id = mp.get_property_native("current-tracks/video/id")
+    local audio_track_id = mp.get_property_native("current-tracks/audio/id")
     local sub_track_id = mp.get_property_native("current-tracks/sub/id")
 
     input.select({
@@ -136,7 +169,7 @@ local function get_params()
                     default_text = "23",
                     submit = function(crf)
                         if tonumber(crf) then
-                            do_clip(a, b, crf, nil, sub_track_id, path)
+                            do_clip(a, b, crf, nil, video_track_id, audio_track_id, sub_track_id, path)
                         else
                             mp.osd_message("Invalid CRF; doing nothing")
                         end
@@ -150,7 +183,7 @@ local function get_params()
                     submit = function(two_pass_target)
                         two_pass_target = tonumber(two_pass_target)
                         if two_pass_target then
-                            do_clip(a, b, nil, two_pass_target, sub_track_id, path)
+                            do_clip(a, b, nil, two_pass_target, video_track_id, audio_track_id, sub_track_id, path)
                         else
                             mp.osd_message("Invalid size, doing nothing")
                         end
