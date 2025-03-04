af900a78fb0ca1315817436e35b531e9905d4a8a
Arjun Satarkar <me@arjunsatarkar.net>
2025-03-04 14:57:29 -0500

    Add license header to source files

--- get_subtitle/main.lua
+++ get_subtitle/main.lua
@@ -1,3 +1,16 @@
+--[[
+mpv scripts - get_subtitle
+Copyright (C) 2025-present Arjun Satarkar
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License version 3 for
+more details.
+]]
+
 Result = nil
 
 local function main()
--- mpvclip/main.lua
+++ mpvclip/main.lua
@@ -1,3 +1,16 @@
+--[[
+mpv scripts - mpvclip
+Copyright (C) 2025-present Arjun Satarkar
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License version 3 for
+more details.
+]]
+
 local input = require "mp.input"
 
 local function log_cmd(args)
